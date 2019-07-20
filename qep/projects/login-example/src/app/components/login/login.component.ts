7d40b2050a15b58168125f7e3c2aa7e368c64818
Imran Rauf <write2imran@yahoo.com>
2019-07-20 11:30:14 +0300

    dummy update

--- qep/projects/login-example/src/app/components/login/login.component.ts
+++ qep/projects/login-example/src/app/components/login/login.component.ts
@@ -54,7 +54,7 @@ export class LoginComponent implements OnInit {
         if (this.loginForm.invalid) {
             return;
         }
-
+ 
         this.loading = true; 
         this.authenticationService.login(this.f.username.value, this.f.password.value)
             .pipe(first())
